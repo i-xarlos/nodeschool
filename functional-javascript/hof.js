2f6f445577d5d915c6122f8b73613e1b1ed6f8fa
Carlos Casa <cgil@ncomunicaciones.com>
2019-02-26 19:29:21 -0500

    -

---
+++ functional-javascript/hof.js
@@ -0,0 +1,12 @@
+
+function repeat(multiply, x) {
+    return x => multiply * x
+  }
+
+  // Do not remove the line below
+  module.exports = repeat
+
+  let doubler = repeat(4);
+
+  console.log(doubler(2));
+  
---
+++ functional-javascript/map.js
@@ -0,0 +1,5 @@
+let doubleAll = (x) => x.map(y => y * 2) 
+module.exports = doubleAll
+
+console.log( doubleAll( [1,2,3,4] ) );
+
