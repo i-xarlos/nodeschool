47c52d311682afc9355f71ebfc5cd1a9526ec063
Carlos <ixarlos@gmail.com>
2019-03-19 17:59:48 -0500

    -

--- functional-javascript/expo.js
+++ functional-javascript/expo.js
@@ -1,3 +1,3 @@
-var ncom = ['Gerson','Christian','Matheu','Carlos']
+var ncom = ['Gerson','Matheu']
 var rand = x => x[ Math.ceil( Math.random()* x.length - 1 )] || 0
 console.log(rand(ncom))
--- functional-javascript/recursive.js
+++ functional-javascript/recursive.js
@@ -1,2 +1,16 @@
-var loremIpsum = require('../node_modules/lorem-ipsum')
-console.log(loremIpsum());
+function reduce ( arr , fn , initial) {
+    return ( function reduceOne ( index, value ) {
+      if ( index > arr.length - 1 ) return value // end condition
+      return reduceOne ( index + 1, fn(value, arr[index], index, arr) ) // calculate & pass values to next step
+    }) ( 0, initial ) // IIFE. kick off recursion with initial values
+  }
+
+
+  module.exports = reduce
+
+ let r =  reduce ( [1,2,3] , function(prev, curr, index, arr) {
+    return prev + curr
+  }, 0)
+
+  console.log(r);
+
