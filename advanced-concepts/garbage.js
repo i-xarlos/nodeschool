60e8193869ae6def9b3deb85eba24e2e6d0cda09
Carlos laptop <ixarlos@gmail.com>
2019-04-11 19:56:19 -0500

    advanced-concepts course

---
+++ advanced-concepts/garbage.js
@@ -0,0 +1,18 @@
+const number = 610
+const string = 'some text'
+const human = {
+    first : 'Andrei',
+    last  : 'Neagoie'
+}
+
+function substractTwo(num){
+    return num - 2
+}
+
+function calculate(){
+    const sumTotal = 4 + 5
+    return substractTwo(sumTotal)
+}
+
+console.log(calculate())
+
